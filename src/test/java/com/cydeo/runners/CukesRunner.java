98db03bc79705f8b01047e6649ce025463a5536f
Bahar Taskin <cakirbhr7@gmail.com>
2023-02-22 23:43:47 +0300

    updated

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,19 +6,27 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-reports.html",
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions",
+        glue = "cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"
-        //tags = "@librarian"
-        //tags = "@student or admin"
-        //tags = "@regression and not student"
-        //- We can use tags to include or exclude some scenarios.
-        //- If we put the @Tag on top of the .feature file, it will effect the whole feature file
-        //- If we put it on top of a Scenario, it will only effect that scenario
-        //- You can have as many tags as you want for a feature file or a scenario
+        tags = "@soccerPlayers",
+        publish = false
+
 )
 
 public class CukesRunner {
 }
+
+//tags = "@librarian"
+//tags = "@student or admin"
+//tags = "@regression and not student"
+//- We can use tags to include or exclude some scenarios.
+//- If we put the @Tag on top of the .feature file, it will effect the whole feature file
+//- If we put it on top of a Scenario, it will only effect that scenario
+//- You can have as many tags as you want for a feature file or a scenario
